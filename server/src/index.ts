8b47716ab18d06ae21a0775e24b617425c58fd18
Eugene Kosenko <eku.grim@gmail.com>
2023-07-11 16:16:12 +0300

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,12 @@
-import {CreateUsers} from './scripts/users';
 import express from 'express';
 import routes from './routes'
 import bodyParser from 'body-parser';
-import UserService from "./services/users";
 
-CreateUsers(1000)
-  .then(() => {
-    const app = express();
-    const PORT = 3000;
+  const app = express();
+  const PORT = 3000;
 
-    app.use(bodyParser.json())
-    app.use('/', routes);
-    app.listen(PORT, () => {
-      console.log(`App listening on port ${PORT}`);
-    });
-    UserService.init();
-  })
-  .catch(err => {
-    console.log(`App can't start. Error: ${err}`);
-  })
+  app.use(bodyParser.json())
+  app.use('/', routes);
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
